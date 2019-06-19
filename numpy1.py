8589c226482991addb0e4ca63a658647b4f36d45
Smriti5 <48525729+Smriti5@users.noreply.github.com>
2019-06-19 09:10:45 +0530

    Update numpy1.py

--- numpy1.py
+++ numpy1.py
@@ -8,3 +8,4 @@ m=int(input())                #To take integer input from user
 n=int(input())
 y=np.random.random((m,n))     #Inserting random elements in 2d array
 np.savetxt('a1.txt',y)        #Saving the array to a file
+np.loadtxt('a1.txt')
